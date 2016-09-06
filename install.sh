95c82b8b09b11bcc899c13d5435d4bc90eca8da6
Rob Ede <robjtede@gmail.com>
2016-09-06 01:14:40 +0100

    add zsh aliases install script

--- install.sh
+++ install.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-# ask ubuntu/centos
-# ask systemd serviced
+# install ZSH
+sudo apt-get install zsh
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# insert primary aliases
+cat zaliases > ~/.zaliases
+
+echo "Do you want firewalld aliases?"
+read fwd
+if [[ $fwd == "y" ]]; then
+  echo >> ~/.zaliases
+  cat partials/zaliases.firewalld >> ~/.zaliases
+fi
+
+
+echo "source ~/.zaliases" >> ~/.zshrc
